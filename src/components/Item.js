bb966addfc6f425ce92f60b71b2d6c64f82a7cfa
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-06-12 14:58:40 -0700

    Completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const buttonColor = isInCart ? 'magenta' : 'yellow';
+
+  function handleAddToCart() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
   return (
-    <li className="">
+    <li className={isInCart ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart} style={{ backgroundColor: buttonColor }}>
+        {isInCart ? 'Remove from Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
